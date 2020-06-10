819df5b46b28787e2518354411ecd978d08b51a2
Mario Alonso <mario.blazter99@gmail.com>
2020-06-09 23:12:28 -0500

    Consumiendo una REST API con AJAX

---
+++ JSONAPI/app.js
@@ -0,0 +1,25 @@
+const cargarPosts = document.querySelector('#cargar').addEventListener('click', cargarAPI);
+
+function cargarAPI(){
+    //crear el objeto
+    const xhr = new XMLHttpRequest();
+    //Abrir la conexión
+    xhr.open('GET', 'http://jsonplaceholder.typicode.com/posts', true);
+    //Carga y leer datos
+    xhr.onload = function(){
+        if(this.status === 200){
+            const respuesta = JSON.parse(this.responseText);
+
+            let contenido = '';
+            respuesta.forEach(function(post){
+                contenido += `
+                    <h3>${post.title}</h3>
+                    <p>${post.body}</p>
+                `;
+            });
+            document.getElementById('listado').innerHTML = contenido;
+        }
+    }
+    //Enviar la conexión
+    xhr.send();
+}
